66d98dea60fda4209f5fb435978e3988efd6cb37
greenislandtim <tim@ruthlesslycreate.com>
2013-04-11 16:43:36 -0500

    with font awesome

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module PlV1
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
