ba9d27773ce37469533105097f674a4ddc29b0f4
greenislandtim <tim@ruthlesslycreate.com>
2013-04-10 19:58:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlV1::Application.config.secret_token = 'cb4edd2371dba0c019eb5bcaaa6bedf2fe36b9c3d858f0ab9b78fa15760d9da38692c5a382467f58ec436a0163835d8c42f4d3bc06adb6105eba5c2c26388015'
