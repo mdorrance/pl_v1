6f2774bd196e82b2a2c64b136051bc0ae30f7694
greenislandtim <tim@ruthlesslycreate.com>
2013-04-11 15:23:47 -0500

    add foundation connect user to promo

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,3 +1,4 @@
 class Promotion < ActiveRecord::Base
   attr_accessible :content, :download_link, :download_name, :email, :map_link, :password, :phone, :photo1, :photo2, :photo3, :photo4, :subtitle, :title, :user_id, :video_embed
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :firstname, :lastname
+  has_many :promotions
 end
